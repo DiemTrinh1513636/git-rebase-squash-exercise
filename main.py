566506d789694ddb32f9c24deb5c8efc99cd21bc
trinhntd <diemtrinhvlm97@gmail.com>
2023-06-15 23:28:59 +0700

    add in main file to get conflict

--- main.py
+++ main.py
@@ -2,5 +2,12 @@ from haiku import get_haiku
 
 s = get_haiku()
 
+<<<<<<< Updated upstream
 print('\nThis is our haiku:\n')
 print(s)
+=======
+print('\nThis is our haiku:\n Add to test conflict')
+print('this is conflict hihi')
+
+print('Hello')
+>>>>>>> Stashed changes
